f592c2c023feda8f453b3791d02c1ed7af7e1edc
Natalie Cheng <nataliemcheng1@gmail.com>
2023-08-25 15:25:15 -0700

    Updated

--- AddressBook/Models/Contact.cs
+++ AddressBook/Models/Contact.cs
@@ -8,26 +8,26 @@ namespace AddressBook.Models
 	{
 		[Required]
 		[Display(Name = "First Name")]
-		public string FirstName { get; set; }
+		public required string FirstName { get; set; }
 
 		[Required]
         [Display(Name = "Last Name")]
-        public string LastName { get; set; }
+        public required string LastName { get; set; }
 
 		[Required]
-		public string Address1 { get; set; }
+		public required string Address1 { get; set; }
         public string? Address2 { get; set; }
-		public string City { get; set; }
-		public string State { get; set; }
+		public required string City { get; set; }
+		public required string State { get; set; }
 
 		[DataType(DataType.PostalCode)]
 		public int Zip { get; set; }
 
 		[DataType(DataType.EmailAddress)]
-		public string Email { get; set; }
+		public required string Email { get; set; }
 
 		[DataType(DataType.PhoneNumber)]
-		public string Phone { get; set; }
+		public required string Phone { get; set; }
 
 		public DateTime Created { get; set; }
 
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,23 +4,23 @@ using AddressBook.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using System.Configuration;
 
-//var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 //// Add services to the container.
-//builder.Services.AddControllersWithViews();
-////builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    //options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseNpgsql(DataUtility.GetConnectionString(builder.Configuration)));
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseNpgsql(DataUtility.GetConnectionString(builder.Configuration)));
 
-////AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-//builder.Services.AddScoped<IImageService, BasicImageService>();
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+builder.Services.AddScoped<IImageService, BasicImageService>();
 
-//var app = builder.Build();
+var app = builder.Build();
 
-////var dbContext = app.Services.CreateScope().ServiceProvider.GetRequiredService();
+// var dbContext = app.Services.CreateScope().ServiceProvider.GetRequiredService<BasicImageService>();
 
-////await dbContext.Database.MigrateAsync();
+// await dbContext.Database.MigrateAsync();
 
 
 // Configure the HTTP request pipeline.
